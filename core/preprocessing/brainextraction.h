9ae9525a74cacbf3050ed02845aa4e48822c2068
Axel <aple@orange.fr>
2026-01-08 12:01:34 +0100

    Mise à jour de CMakeLists et classe BrainExtraction
    - Ajout de nouveaux fichiers dans `add_library` dans `CMakeLists.txt`, y compris `animawrapper` et `resampling`.
    - Suppression du fichier temporaire `dummy.cpp`.
    - Mise à jour de `target_include_directories` pour inclure de nouveaux répertoires.
    - Ajout de `Qt6::Core` dans `target_link_libraries`.
    - Ajout du membre privé `MainWindow* m_gui;` dans la classe `BrainExtraction`.
    - Résolution nécessaire des conflits de fusion présents dans le code.

--- core/preprocessing/brainextraction.h
+++ core/preprocessing/brainextraction.h
@@ -50,10 +50,6 @@ class BrainExtraction : public QObject {
 
     private: 
       AnimaWrapper* m_wrapper;
-<<<<<<< HEAD
-=======
-      MainWindow* m_gui; 
->>>>>>> 12-preprocessor
       QString m_atlasImage;
       QString m_iccImage;
       QStringList m_pyramidOption;
