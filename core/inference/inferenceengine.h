d1d651abd3915a228a4911a1d5e1462c279502ab
Laïcan <167437065+damien-vailland@users.noreply.github.com>
2026-01-14 12:05:34 +0100

    Inference fonctionnelle

--- core/inference/inferenceengine.h
+++ core/inference/inferenceengine.h
@@ -16,4 +16,4 @@ public:
         const QString& outputName = "output"
     );
 
-};
+}; //
--- gui/main.cpp
+++ gui/main.cpp
@@ -18,14 +18,24 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setOrganizationName("Empenn - INRIA");
     QCoreApplication::setApplicationName("StrokeSeg2");
 
+    // TEST INFERENCE ENGINE
+    QString modelPath = QStringLiteral("C:/ProgramData/StrokeSeg/Models/Monomodal.onnx");
 
-    InferenceEngine engine; // To ensure static initialization
-    auto output = engine.RunInference(
-        "C:/ProgramData/StrokeSeg/Models/Bimodal.onnx",
-        "C:/Users/dvail/Desktop/Laïcan/Code/Pindus/MiniATLAS_2/BIDS/sub-r001s003/anat/sub-r001s003_T1w.nii.gz"
-    );
+    QString imagePath = QStringLiteral("C:/Users/dvail/source/repos/MiniATLAS_2/BIDS/sub-r001s003/anat/sub-r001s003_T1w.nii.gz");
+    //QString imagePath = QStringLiteral("C:/Users/dvail/source/repos/MiniATLAS_2/BIDS/derivatives/sub-r001s001/anat/sub-r001s001_T1w_MNI.nii.gz");
+    //QString imagePath = QStringLiteral("C:/Users/dvail/source/repos/MiniATLAS_2/BIDS/derivatives/sub-r001s005/anat/sub-r001s005_T1w_BET.nii.gz");
+    
+    InferenceEngine engine;
+    qDebug() << "Loading for image :" << imagePath;
 
-    qDebug() << "Output size =" << output.size();
+    auto output = engine.RunInference(modelPath, imagePath, "input", "output");
+
+    if (output.empty()) {
+        qDebug() << "Failure : no data out";
+    } else {
+        qDebug() << "Success ! Output size =" << output.size();
+    }
+    // FIN TEST
 
     MainWindow w;
     w.show();
